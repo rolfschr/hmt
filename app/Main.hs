b2d312e9e2e212b1ecbebf8b580c7bbfe82405bb
Rolf Schröder <rolf.schr@gmail.com>
2018-09-10 11:22:35 +0200

    Allow to read from files provided on the command line

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,34 @@
 module Main where
 
-import LibHmt
-import System.Exit
+import Data.List
+import System.Directory
 import System.Environment
+import System.Exit
+import Control.Monad.Extra
+
+import LibHmt
 
 -- TODO add version + git hash to ouptut , see https://haskell-lang.org/library/optparse-applicative
+
+-- Split the input Strings into a list of (existing) files and the remainder.
+extractFiles :: [FilePath] -> IO ([FilePath], [FilePath])
+extractFiles args = partitionM doesFileExist args
+
+-- Read input to be fed to hmt. Either from files provided on the command line
+-- or from stdin.
+getInput :: [String] -> IO String
+getInput args = do
+    (files, otherArgs) <- extractFiles args
+    if (length files) > 0
+        then concatMapM readFile files
+        else getContents
 
 main :: IO ()
 main = do
     args <- getArgs
-    (exitcode, stdout, stderr) <- getContents >>= hmtWith "fmt" args
+    (files, otherArgs) <- extractFiles args
+    stdin <- getInput args
+    (exitcode, stdout, stderr) <- hmtWith "fmt" otherArgs stdin
     if (exitcode == ExitSuccess)
         then putStr stdout
         else putStr $ (show exitcode) ++ ": " ++ stderr
